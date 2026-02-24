19686656a97a0f797f0198fa1af2f7839227cd07
Anne Lin <100283618+anne-lin-magna@users.noreply.github.com>
2026-02-24 13:44:19 +0200

    Update

--- service-worker.js
+++ service-worker.js
@@ -2,14 +2,8 @@
 
 self.addEventListener("install", (event) => {
     console.log('Service Worker: Installing...');
-    self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
     console.log('Service Worker: Activating...');
-    event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', () => {
-    // Listener registered — required for PWA installability
 });
